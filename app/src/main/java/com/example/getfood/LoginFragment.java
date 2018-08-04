7f3aef35f6783d270dd49f67ea423bd97dba4305
Adit Modhvadia <adit.modhvadia@gmail.com>
2018-08-04 20:43:18 +0530

    add launcher activiity for login and register

---
+++ app/src/main/java/com/example/getfood/LoginFragment.java
@@ -0,0 +1,35 @@
+package com.example.getfood;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class LoginFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_curr_user, container, false);
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
