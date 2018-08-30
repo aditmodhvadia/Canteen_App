82e1c112e16e3f279d83ff8bb8bfdbad0aa5f18a
Adit Modhvadia <adit.modhvadia@gmail.com>
2018-08-30 10:51:05 +0530

    fix position of fab for logout

--- app/src/main/java/com/example/getfood/FoodMenuDisplayActivity.java
+++ app/src/main/java/com/example/getfood/FoodMenuDisplayActivity.java
@@ -79,7 +79,6 @@ public class FoodMenuDisplayActivity extends AppCompatActivity {
 
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
