08d39594716eb57811461b450c8aaed286cb622a
Adit Modhvadia <adit.modhvadia@gmail.com>
2018-08-31 00:48:22 +0530

    add Cart ListView display

--- app/src/main/java/com/example/getfood/CartActivity.java
+++ app/src/main/java/com/example/getfood/CartActivity.java
@@ -3,6 +3,7 @@ package com.example.getfood;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -11,11 +12,15 @@ public class CartActivity extends AppCompatActivity {
 
     ArrayList<String> cartItemName;
     ArrayList<Integer> cartItemQuantity, cartItemPrice;
+    ListView cartListView;
+    CartDisplayAdapter cartDisplayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        cartListView = findViewById(R.id.cartListView);
 
         cartItemName = new ArrayList<>();
         cartItemPrice = new ArrayList<>();
@@ -25,6 +30,10 @@ public class CartActivity extends AppCompatActivity {
         cartItemName = (ArrayList<String>) i.getExtras().getStringArrayList("name").clone();
         cartItemPrice = (ArrayList<Integer>) i.getExtras().getStringArrayList("price").clone();
         cartItemQuantity = (ArrayList<Integer>) i.getExtras().getStringArrayList("quantity").clone();
+
+        cartDisplayAdapter = new CartDisplayAdapter(cartItemName,cartItemQuantity,cartItemPrice,getApplicationContext());
+        cartListView.setAdapter(cartDisplayAdapter);
+
         Toast.makeText(getApplicationContext(),cartItemName.toString(),Toast.LENGTH_LONG).show();
     }
 }
