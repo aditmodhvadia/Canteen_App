7f833ef245ae14777215cb06f6e3807e62b4defd
Adit Modhvadia <adit.modhvadia@gmail.com>
2019-01-07 01:40:45 +0530

    - add splash activity with logo - removed files not in use

---
+++ app/src/main/java/com/example/getfood/Activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.getfood.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.getfood.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
