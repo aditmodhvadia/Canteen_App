91a64cfa8cb6ee2622a490d583263306ebc6b071
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2019-06-11 06:55:35 +0530

    - add common orderListListener to FireBaseApiManager

--- app/src/main/java/com/example/getfood/api/FireBaseApiManager.java
+++ app/src/main/java/com/example/getfood/api/FireBaseApiManager.java
@@ -26,11 +26,41 @@ public class FireBaseApiManager {
 //    TODO: Implement common call/listener for all FireBase RealTime Database listeners first and
 //     then write calls and all FireBase APIs subsequently
 
-
+    /**
+     * Call to fetch details of an Orders placed by a User
+     *
+     * @param rollNo        Roll Number of User
+     * @param orderID       Unique Order ID
+     * @param eventListener Callback for ValueEventListener
+     */
     public void orderDetailListener(@NonNull String rollNo, @NonNull String orderID, final ValueEventListener eventListener) {
 
         DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference()
                 .child(BaseUrl.USER_ORDER.url).child(rollNo).child(orderID);
+
+        apiWrapper.valueEventListener(dbRef, new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                eventListener.onDataChange(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                eventListener.onCancelled(databaseError);
+            }
+        });
+    }
+
+    /**
+     * Call to fetch List of all Orders placed by a User
+     *
+     * @param rollNo        Roll Number of User
+     * @param eventListener Callback for ValueEventListener
+     */
+    public void orderListListener(@NonNull String rollNo, final ValueEventListener eventListener) {
+
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference()
+                .child(BaseUrl.USER_ORDER.url).child(rollNo);
 
         apiWrapper.valueEventListener(dbRef, new ValueEventListener() {
             @Override
--- app/src/main/java/com/example/getfood/ui/orderlist/OrderListPresenter.java
+++ app/src/main/java/com/example/getfood/ui/orderlist/OrderListPresenter.java
@@ -2,19 +2,15 @@ package com.example.getfood.ui.orderlist;
 
 import android.support.annotation.NonNull;
 
-import com.example.getfood.R;
 import com.example.getfood.models.FullOrder;
 import com.example.getfood.ui.base.BasePresenter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class OrderListPresenter<V extends OrderListMvpView> extends BasePresenter<V> implements OrderListMvpPresenter<V> {
-    private DatabaseReference orderData, orderRoot, userOrderData;
     private ArrayList<FullOrder> orderListItems;
 
     public OrderListPresenter() {
@@ -22,18 +18,12 @@ public class OrderListPresenter<V extends OrderListMvpView> extends BasePresente
 
     @Override
     public void fetchOrderList(String rollNo) {
-
-        orderData = FirebaseDatabase.getInstance().getReference().child(getMvpView().getContext().getString(R.string.order_data));
-        orderRoot = FirebaseDatabase.getInstance().getReference().child(getMvpView().getContext().getString(R.string.order));
-        userOrderData = FirebaseDatabase.getInstance().getReference().child("UserOrderData");
-
         orderListItems = new ArrayList<>();
 
-        userOrderData.child(rollNo).addValueEventListener(new ValueEventListener() {
+        apiManager.orderListListener(rollNo, new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-
                     orderListItems.clear();
                     for (DataSnapshot dsp : dataSnapshot.getChildren()) {
                         orderListItems.add(dsp.getValue(FullOrder.class));
