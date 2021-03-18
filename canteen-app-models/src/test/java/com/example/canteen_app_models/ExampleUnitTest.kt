89fd09e173d163e5d93ad811ef1a04200afd0e70
Adit Modhvadia <adit.modhvadia@gmail.com>
2021-03-18 11:05:20 -0400

    - migrate canteen-app-models library

--- canteen-app-models/src/test/java/com/example/canteen_app_models/ExampleUnitTest.kt
+++ canteen-app-models/src/test/java/com/example/canteen_app_models/ExampleUnitTest.kt
@@ -1,17 +1,16 @@
-package com.example.canteen_app_models;
+package com.example.canteen_app_models
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.Assert
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, (2 + 2).toLong())
     }
 }
